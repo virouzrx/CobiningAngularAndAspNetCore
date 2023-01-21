cbb3b969db50f4d40e801cc90d0de9f7e71ea2e1
virouzrx <szymon.szalata.1@gmail.com>
2023-01-21 17:10:45 +0100

    Initial commit

---
+++ AngularAndMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AngularAndMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
